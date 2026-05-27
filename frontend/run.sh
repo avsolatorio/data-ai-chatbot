f6ab6c13a004072f1cbbf26ed7ea3e8d9e0419e7
Aivin V. Solatorio <avsolatorio@gmail.com>
2026-05-26 20:22:52 -0400

    Add run script for frontend
    Signed-off-by: Aivin V. Solatorio <avsolatorio@gmail.com>

--- frontend/run.sh
+++ frontend/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-cd "$(dirname "$0")" && pnpm dev --port 3001
+# cd "$(dirname "$0")" && pnpm dev --port 3001
+cd "$(dirname "$0")" && pnpm install && pnpm build:local && PORT=3001 node .next/standalone/server.js
